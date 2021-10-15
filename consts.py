22eec7d05ce36fac3639a71fe4c5b58feba9f52c
matanhaller <matan.haller@gmail.com>
2021-10-15 16:06:16 +0300

    Add universal constants

---
+++ consts.py
@@ -0,0 +1,11 @@
+"""Universal constants for the infrastructure.
+"""
+
+from scipy.constants import physical_constants
+
+c = physical_constants['speed of light in vacuum']  # m/s
+hbar = physical_constants['reduced Planck constant']  # J*s
+e = physical_constants['elementary charge']  # C
+m_e = physical_constants['electron mass']  # kg
+epsilon_0 = physical_constants['vacuum electric permittivity']  # F*m^-1
+mu_0 = physical_constants['vacuum mag. permeability']  # N*A^-2
