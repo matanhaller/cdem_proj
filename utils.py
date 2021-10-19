8c0e2912cc36632f516500bd7434d43a839457f0
matanhaller <matan.haller@gmail.com>
2021-10-19 12:53:49 +0300

    Add shift functionality to FFT

--- utils.py
+++ utils.py
@@ -5,56 +5,52 @@ import numpy as np
 from scipy.fft import fft, fftshift, ifft, ifftshift
 
 
-def T(v):
-    """Implement transpose for row vector by reshaping to len(v) x 1.
-
-    Args:
-        v (np.array): Row vector to be transposed.
-
-    Returns:
-        np.array: len(v) x 1 column vector of v^T.
-    """
-    return np.reshape(v, (len(v), 1))
-
-
-def ctft(f, x_0, delta_x):
+def ctft(f, x_0, delta_x, shift=True):
     """Approximate implementation of the "physical" Fourier transform
-    (integral from -Inf to Inf).
+    (integral).
 
     Args:
         f (np.array): Array to be transformed.
         x_0 (float): Leftmost sampling point.
         delta_x (float): Sampling interval.
+        shift (bool): Whether to shift zero-frequency component to center.
 
     Returns:
         np.array: Array of length N of the "physical" Fourier transform of f.
     """
-
     N = len(f)
     n = np.arange(-np.floor(N / 2), np.ceil(N / 2))
     k_0 = 2 * np.pi / (N * delta_x)
     phase_fix = np.exp(-1j * k_0 * x_0 * n)
 
-    return phase_fix * fftshift(fft(f)) * delta_x
+    F = fft(f)
+    if shift:
+        F = fftshift(F)
+
+    return (1 / (2 * np.pi)) * phase_fix * F * delta_x
 
 
-def ctift(f, x_0, delta_x):
+def ctift(f, x_0, delta_x, shift=True):
     """Approximate implementation of the "physical" inverse Fourier transform
-    (integral from -Inf to Inf).
+    (integral).
 
     Args:
         f (np.array): Array to be transformed.
         x_0 (float): Leftmost sampling point.
         delta_x (float): Sampling interval.
+        shift (bool): Whether to shift zero-frequency component to center.
 
     Returns:
         np.array: Array of length N of the "physical" inv. Fourier transform
         of f.
     """
-
     N = len(f)
     n = np.arange(-np.floor(N / 2), np.ceil(N / 2))
     k_0 = 2 * np.pi / (N * delta_x)
     phase_fix = np.exp(1j * k_0 * x_0 * n)
 
-    return N * phase_fix * ifftshift(ifft(f)) * delta_x
+    F = ifft(f)
+    if shift:
+        F = ifftshift(F)
+
+    return N * phase_fix * F * delta_x
