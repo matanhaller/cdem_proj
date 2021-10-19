110fdefc2e0ed428c3c968a7275c05610e81a750
matanhaller <matan.haller@gmail.com>
2021-10-19 12:54:06 +0300

    Fix error in referring to constants

--- consts.py
+++ consts.py
@@ -3,9 +3,9 @@
 
 from scipy.constants import physical_constants
 
-c = physical_constants['speed of light in vacuum']  # m/s
-hbar = physical_constants['reduced Planck constant']  # J*s
-e = physical_constants['elementary charge']  # C
-m_e = physical_constants['electron mass']  # kg
-epsilon_0 = physical_constants['vacuum electric permittivity']  # F*m^-1
-mu_0 = physical_constants['vacuum mag. permeability']  # N*A^-2
+c = physical_constants['speed of light in vacuum'][0]  # m/s
+hbar = physical_constants['reduced Planck constant'][0]  # J*s
+e = physical_constants['elementary charge'][0]  # C
+m_e = physical_constants['electron mass'][0]  # kg
+epsilon_0 = physical_constants['vacuum electric permittivity'][0]  # F*m^-1
+mu_0 = physical_constants['vacuum mag. permeability'][0]  # N*A^-2
